814f9a1702cf298d9228991acf40ddd177c5c540
Yekeen Abiodun <abiodunyekeen76@gmail.com>
2023-12-02 02:37:41 +0000

    update app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Abbey.
+          Edit <code>src/App.js</code> and save to reload Abbey .
+          <span>Everything is working perfectly</span>
         </p>
         <a
           className="App-link"
